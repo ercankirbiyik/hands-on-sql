f68ffacb0006618b18b9e9be53d8b13d2db658fe
Ercan Kırbıyık <rcnkirbiyik@gmail.com>
2022-12-16 22:16:15 +0000

    new examples added

--- queries/01_03/CharacterFunctions.sql
+++ queries/01_03/CharacterFunctions.sql
@@ -1,13 +1,25 @@
 SELECT UPPER('HelloWorld');
 
+SELECT UPPER('My name is Ercan');
+
 SELECT LOWER('HelloWorld');
+
+SELECT LOWER('MY SURNAME IS KIRBIYIK');
 
 SELECT SUBSTR('HelloWorld1234567890',6,10);
 
+SELECT SUBSTR('ERCAN KIRBIYIK',3,7);
+
 SELECT LENGTH('HelloWorld');
+
+SELECT LENGTH('SQL LEARNING CLASS');
 
 SELECT INSTR('HelloWorld','e');
 
+SELECT INSTR('ERCAN','R');
+
 SELECT TRIM('Hello World','World');
+
+SELECT TRIM('Günaydın benden içeri Istanbullll','Istanbullll büyüktür benden');
 
 SELECT REPLACE('Hello World','Hello','Good morning');
--- queries/01_03/ConditionalFunctions.sql
+++ queries/01_03/ConditionalFunctions.sql
@@ -5,3 +5,14 @@ CASE job WHEN 'MANAGER' THEN 1.20*salary
          ELSE salary
          END 'Revised Salary'
 FROM emp_tab;
+
+SELECT name,job,salary FROM emp_tab;
+
+SELECT name,job,salary,
+CASE job WHEN 'PRESIDENT' THEN 5*salary
+         WHEN 'MANAGER' THEN 10*salary
+         WHEN 'ANALYST' THEN 15*salary
+         WHEN 'CLERK' THEN   25*salary
+         ELSE salary
+         END 'Revised Salary'
+FROM emp_tab;
