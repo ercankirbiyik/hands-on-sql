ab7084a3ea9265637367b3e29d6ad9dabe3b459b
Ercan Kırbıyık <rcnkirbiyik@gmail.com>
2022-12-17 11:13:27 +0000

    new examples added for GroupFunctions

--- queries/01_04/GroupFunctions.sql
+++ queries/01_04/GroupFunctions.sql
@@ -1,11 +1,22 @@
+SELECT * FROM emp_tab;
+
 --Sum of all salaries
 SELECT SUM(salary) FROM emp_tab;
+
+--Sum of all commission
+SELECT SUM(commission) FROM emp_tab;
 
 --Average of all salaries
 SELECT AVG(salary) FROM emp_tab;
 
+--Average of all commission
+SELECT AVG(commission) FROM emp_tab;
+
 --Minimum and Maximum salaries for employees in department 30
 SELECT MIN(salary),MAX(salary) FROM emp_tab WHERE deptno=30;
+
+--Minimum and Maximum salaries for employees in department 40
+SELECT MIN(salary),MAX(salary) FROM emp_tab WHERE deptno=40;
 
 --The oldest and the most recently joined employees hire dates 
 SELECT MIN(hiredate),MAX(hiredate) FROM emp_tab;
@@ -20,11 +31,15 @@ SELECT COUNT(*) FROM emp_tab;
 SELECT COUNT(name) FROM emp_tab 
 WHERE manager=7003;
 
+--The number of employees reporting to manager 7001
+SELECT COUNT(name) FROM emp_tab 
+WHERE manager=7001;
+
 --Counts the distinct empno in the table
 SELECT COUNT(DISTINCT(empno)) FROM emp_tab;
 
 --Sorts the highest salaries under every manager in descending order
-SELECT MAX(salary),manager 
+SELECT MAX(salary),manager, name, deptno
 FROM emp_tab 
 GROUP BY manager 
 ORDER BY MAX(salary) DESC;
@@ -32,11 +47,11 @@ ORDER BY MAX(salary) DESC;
 --Returns an error as group functions cannot be given in WHERE condition
 SELECT deptno, MAX(salary) 
 FROM emp_tab 
-WHERE MAX(salary)>=3000 
+WHERE MAX(salary)>=3000 --cannot be given in WHERE condition, HAVING condition will be uses
 GROUP BY deptno;
 
 --Fetches highest salary in every department
-SELECT deptno, MAX(salary) 
+SELECT deptno,manager, MAX(salary) 
 FROM emp_tab 
 GROUP BY deptno;
 
@@ -45,3 +60,10 @@ SELECT deptno, MAX(salary)
 FROM emp_tab 
 GROUP BY deptno 
 HAVING MAX(salary)>3000;
+
+
+--Fetches highest salary in every department if its greater than 3000
+SELECT deptno,name, MAX(salary) 
+FROM emp_tab 
+GROUP BY deptno 
+HAVING MAX(salary)>2000;
