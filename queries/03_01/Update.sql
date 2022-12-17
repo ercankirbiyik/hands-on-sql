64e8c8a2a62afd4b43dbb1f2ec9458d539f36fc0
Ercan Kırbıyık <rcnkirbiyik@gmail.com>
2022-12-17 18:16:36 +0000

    new examples added

--- queries/03_01/Update.sql
+++ queries/03_01/Update.sql
@@ -1,7 +1,13 @@
+SELECT * FROM emp_tab;
+
 --Modify one row in emp_tab
 UPDATE emp_tab 
 SET salary=8000 
 WHERE empno=7001;
+
+UPDATE emp_tab 
+SET salary=5000 
+WHERE empno=7006;
 
 --Modify multiple columns using subquery
 UPDATE emp_tab 
@@ -16,6 +22,9 @@ salary = (
 WHERE empno=7002;
 
 --Modify row in a different table using subquery
+
+SELECT * FROM old_emp_tab;
+
 UPDATE old_emp_tab 
 SET deptno = (
   SELECT deptno 
